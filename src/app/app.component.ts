9d29152941ef1c2d71bee780762d6f1456e33e80
mchugh91 <ciaranmchugh7@hotmail.com>
2021-12-06 15:58:58 -0800

    Revert "first-commit"
    This reverts commit eacff905c94b7a8dc3586f83b535754b6999bcf2.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,10 @@
 import { Component } from '@angular/core';
-import { RecipeService } from './recipe/recipe.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-  providers: [RecipeService]
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'course-project';
-  showRecipes: boolean = true;
-  showShoppingList: boolean = false;
-
-  // displayChange(type: string){
-  //   if(type === 'recipes'){
-  //     this.showRecipes = true;
-  //     this.showShoppingList = false;
-  //   }else if (type === 'shopping'){
-  //     this.showShoppingList = true;
-  //     this.showRecipes = false
-  //   }
-  // }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,40 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { ShoppingListComponent } from './shopping-list-parent/shopping-list/shopping-list.component';
-import { ShoppingListEditComponent } from './shopping-list-parent/shopping-list-edit/shopping-list-edit.component';
-import { IngredientComponent } from './shopping-list-parent/ingredient/ingredient.component';
-import { RecipeListComponent } from './recipe/recipe-list/recipe-list.component';
-import { RecipeItemComponent } from './recipe/recipe-item/recipe-item.component';
-import { RecipeDetailComponent } from './recipe/recipe-detail/recipe-detail.component';
-import { RecipeComponent } from './recipe/recipe.component';
-import { DropdownDirective } from './shared/dropdown.directive';
-import { ShoppingListService } from './shopping-list-parent/shopping-list.service';
-import { AppRoutingModule } from './app-routing.module';
-import { RecipeStartComponent } from './recipe/recipe-start/recipe-start.component';
-import { RecipeEditComponent } from './recipe/recipe-edit/recipe-edit.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HeaderComponent,
-    ShoppingListComponent,
-    ShoppingListEditComponent,
-    IngredientComponent,
-    RecipeListComponent,
-    RecipeItemComponent,
-    RecipeDetailComponent,
-    RecipeComponent,
-    DropdownDirective,
-    RecipeStartComponent,
-    RecipeEditComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule, AppRoutingModule, FormsModule, ReactiveFormsModule
+    BrowserModule
   ],
-  providers: [ShoppingListService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
